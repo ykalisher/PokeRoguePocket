7fba8303a87ec45dc2a2380a62f364b8b6a98b18
Yoav Kalisher <ykalisher@gmail.com>
2026-05-15 11:58:39 +0200

    Added flask as a requirement

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
 from flask import Flask, redirect, render_template, url_for, abort
 from json import load
 app = Flask(__name__, static_url_path='/static')
-
-with open('data.json', 'r') as f:
-	data = load(f)
 	
 @app.get('/')
 def index():
 	return render_template('index.html')
 	
 @app.get('/game')
-def index():
+def game():
 	return render_template('index.html')
 
 @app.errorhandler(404)
